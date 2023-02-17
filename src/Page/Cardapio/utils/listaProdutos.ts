693c4a9b7e7f0cb1dac4295db66b26369180cd0d
victor077 <victor.souza@bandtec.com.br>
2023-02-17 17:30:58 -0300

    ajustes pastas

---
+++ src/Page/Cardapio/utils/listaProdutos.ts
@@ -0,0 +1,101 @@
+export const paoDeMelTrufados = [
+  {
+    id: 1,
+    sabor: "Nutela",
+    tamanhoP: "R$9",
+    tamanhoM: "R$12",
+  },
+  {
+    id: 2,
+    sabor: "Doce de Leite",
+    tamanhoP: "R$8",
+    tamanhoM: "R$11",
+  },
+  {
+    id: 3,
+    sabor: "Laka Oreo",
+    tamanhoP: "R$8",
+    tamanhoM: "R$11",
+  },
+];
+
+export const ovosDeColher = [
+  {
+    id: 1,
+    sabor: "Maracuja",
+    kilo50: "R$11",
+    kilo200: "R$21",
+  },
+  {
+    id: 2,
+    sabor: "Laka Oreo",
+    kilo50: "R$11",
+    kilo200: "R$21",
+  },
+  {
+    id: 3,
+    sabor: "Ouro Branco",
+    kilo50: "R$12",
+    kilo200: "R$22",
+  },
+  {
+    id: 4,
+    sabor: "Ninho c/Nutella",
+    kilo50: "R$14",
+    kilo200: "R$25",
+  },
+  {
+    id: 5,
+    sabor: "Ferrero Rocher",
+    kilo50: "R$14",
+    kilo200: "R$25",
+  },
+];
+
+export const miniTrufasDeAcucar = [{
+  id: 1,
+  sabor: 'Ninho c/ Nutela',
+  valor: 'R$4,50'
+},
+{
+  id: 2,
+  sabor: 'Ouro Branco',
+  valor: 'R$4,50'
+}, {
+  id: 3,
+  sabor: 'Mouse de Maracujá',
+  valor: 'R$4,50'
+}, {
+  id: 4,
+  sabor: 'Caixinha com 04 unidades',
+  valor: 'R$16,50'
+},
+{
+  id: 5,
+  sabor: 'Lata personalizada 07 unidades',
+  valor: 'R$35,50'
+},]
+
+export const coneRecheado = [{
+  id: 1,
+  sabor: 'Nutella',
+  tamanhoP: "R$9",
+  tamanhoM: "R$12",
+}, {
+  id: 2,
+  sabor: 'Ninho c/nutella',
+  tamanhoP: "R$9",
+  tamanhoM: "R$12",
+},
+{
+  id: 3,
+  sabor: 'Doce de Leite',
+  tamanhoP: "R$7",
+  tamanhoM: "R$10",
+},
+{
+  id: 4,
+  sabor: 'Laka Oreo',
+  tamanhoP: "R$7",
+  tamanhoM: "R$10",
+},]
